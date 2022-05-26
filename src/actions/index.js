b1ff54705763669dcf10208f3f21f643313be7b0
AkshayBora <akshaybora5@gmail.com>
2022-05-26 20:48:10 +0530

    Actions Creater

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,3 +2,14 @@
 //     type: 'ADD_MOVIES'
 //     movies: [m1,m2]
 // }
+
+// actions types
+export const ADD_MOVIES = 'ADD_MOVIES';
+
+// actions creaters
+export function addMovies (movies) {
+    return {
+        type: ADD_MOVIES,
+        movies // shorthand movies : movies
+    }
+} 
