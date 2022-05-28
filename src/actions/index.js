8cab2c6f12440acd8266fedb5b1bdca65c432ba6
AkshayBora <akshaybora5@gmail.com>
2022-05-28 15:52:41 +0530

    Favouriting movies in App

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,10 +6,19 @@
 // actions types
 export const ADD_MOVIES = 'ADD_MOVIES';
 
+export const ADD_FAVOURITE = 'ADD_FAVOURITE';
+
 // actions creaters
 export function addMovies (movies) {
     return {
         type: ADD_MOVIES,
         movies // shorthand movies : movies
+    }
+} 
+
+export function addFavourite (movie) {
+    return {
+        type: ADD_FAVOURITE,
+        movie // shorthand movies : movies
     }
 } 
