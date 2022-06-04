2e01c0ac293582c7016307dfa44520fe0e4f5a68
AkshayBora <akshaybora5@gmail.com>
2022-06-04 21:42:46 +0530

    Adding Search result in Store

--- src/actions/index.js
+++ src/actions/index.js
@@ -14,6 +14,8 @@ export const SET_SHOW_FAVOURITES = 'SET_SHOW_FAVOURITES';
 
 export const ADD_MOVIE_TO_LIST = 'ADD_MOVIE_TO_LIST';
 
+export const ADD_SEARCH_RESULT= 'ADD_SEARCH_RESULT';
+
 // actions creaters
 export function addMovies (movies) {
     return {
@@ -56,9 +58,19 @@ export function handleMovieSearch(movie) {
     return function (dispatch) {
         fetch(url)
             .then(response => response.json())
-            .then(movie => {
+            .then(movie =>
+            {
                 console.log('movie', movie);
                 //dispatch an action
-            })
-    }
+                //{type:ADD_SEARCH_RESULT, movie}
+                dispatch(addMovieSearchResult(movie));
+            });
+    };
 } 
+
+export function addMovieSearchResult(movie) {
+    return {
+        type: ADD_SEARCH_RESULT,
+        movie
+    }
+}
